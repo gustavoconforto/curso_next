16c30b8a244a60184af2a53c72bb24bfb6c11705
Gustavo Conforto <gustavoconforto@gmail.com>
2026-06-27 10:20:26 -0300

    (feat:migrations) migrations first implementation

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,14 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-  await client.connect();
+  let client;
   try {
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
@@ -29,6 +23,21 @@ function getSSLValues() {
   return process.env.NODE_ENV === "production" ? true : false;
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
